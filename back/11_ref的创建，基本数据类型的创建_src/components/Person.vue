4ae8edb68b00d4c2ddc90e37cbda60425726ad96
nanchengcyu <1146137024@qq.com>
2024-07-16 15:57:35 +0800

    first commit

---
+++ back/11_ref的创建，基本数据类型的创建_src/components/Person.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+let name = ref('南城余')
+let age = ref(18)
+
+function changeName() {
+  name.value = '南城'
+}
+
+function changeAge() {
+  age.value = 22
+}
+
+</script>
+
+<template>
+  名字：{{ name }}
+  年龄：{{ age }}
+  <button @click="changeName">修改名字</button>
+  <br>
+  <button @click="changeAge">修改年龄</button>
+</template>
+
+<style scoped>
+
+</style>
---
+++ back/12_reactive对象式响应数据/components/Person.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import {reactive} from "vue";
+
+let games = reactive([
+  {id: '0001', name: '小南'},
+  {id: '0002', name: '小城'},
+  {id: '0003', name: '小余'},
+])
+
+</script>
+
+<template>
+  <div class="person">
+  <!--reactive对象式相应数据-->
+  <ul>
+    <li v-for="game in games" :key="game.id">{{game.name}}</li>
+  </ul>
+  </div>
+</template>
+
+<style scoped>
+.person{
+  background-color: skyblue;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+</style>
